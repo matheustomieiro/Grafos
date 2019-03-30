44c30efe69c35a9108fdeb22b2ecdd351912c409
Matheus <matheus@Matheuss-Air.home>
2019-03-30 14:11:38 -0300

    Updated C file

--- plagio.c
+++ plagio.c
@@ -6,12 +6,12 @@
 int main(int argc, char const *argv[]) {
     system ("python plagio.py"); // System call to run py script
     char word[255];
-    int n = 2; // Two files will be compared
+    int n = 2; // Two files that will be compared
     FILE *txt;    
     txt = fopen("TEXT_0.txt","r"); // Opening file in reading mode
-    for(int i=0; i<n; i++){
-        while(fscanf(txt,"%s",word) != EOF){
-            printf("%s\n",word);
+    for(int i=0; i<n; i++){ // Running for_loop 'n' times
+        while(fscanf(txt,"%s",word) != EOF){ //Reading all words in file
+            printf("%s\n",word); // Just showing. Need to add 'grafo.c'
         }
     fclose(txt);
     printf("\n\t\tNEW FILE\n");
