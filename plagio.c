c85c4016a3ef89e5f22989fb860377a1f3dd5950
Matheus <matheus@Matheuss-Air.home>
2019-03-30 01:32:56 -0300

    Adding Python script

--- plagio.c
+++ plagio.c
@@ -1,1 +1,16 @@
 /*Beginning*/
+#include <Python/Python.h>
+#include <string.h>
+
+int main(int argc, char const *argv[]) {
+    FILE* file;
+    char root[100];
+    strcpy("root","/Users/matheus/git/Grafos/plagio.py");
+    Py_SetProgramName("plagio.py");
+    Py_Initialize();
+    file = fopen("root","r");
+    PyRun_SimpleFile(file, "root");
+    Py_Finalize();
+    fclose(file);
+    return 0;
+}
--- plagio.py
+++ plagio.py
@@ -1,1 +1,29 @@
-#Beginning#
+#Importing Biblio
+import nltk
+
+#Function to include words in file
+def include_words(tagged):
+    for x in range(0, len(tagged)):
+        if any(
+                [tagged[x][1] == "FW", tagged[x][1] == "IN", tagged[x][1] == "JJ",
+                 tagged[x][1] == "JJS", tagged[x][1] == "JJR", tagged[x][1] == "NN",
+                 tagged[x][1] == "NNP", tagged[x][1] == "NNPS", tagged[x][1] == "NNS",
+                 tagged[x][1] == "RB", tagged[x][1] == "RP", tagged[x][1] == "UH",
+                 tagged[x][1] == "VB", tagged[x][1] == "VBD", tagged[x][1] == "VBG",
+                 tagged[x][1] == "VBN", tagged[x][1] == "VBP", tagged[x][1] == "VBZ"]
+        ):
+            f.write(str(tagged[x][0]) + '\n')
+    return
+
+#Running two times
+for y in range(0, 2):
+    #Reading a phrase
+    phrase= raw_input('Type a phrase: ')
+    phrase = nltk.word_tokenize(phrase)
+    phrase = nltk.pos_tag(phrase)
+    #Opening and saving on file
+    f = open(("TEXT_" + str(y) + ".txt"), "w+")
+    include_words(phrase)
+    #Closing
+    f.close()
+exit()
